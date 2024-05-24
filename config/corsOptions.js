7f5d582d51bbc8bd627cd204e3764d8f16113d7b
ucheogbodo-25 <ucheogbodo25@gmail.com>
2024-05-24 08:25:04 -0700

    adding schemas, routes and controllers

---
+++ config/corsOptions.js
@@ -0,0 +1,22 @@
+const allowedOrigins = [
+  "https://UniTrack.onrender.com",
+  "http://localhost:3000",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (
+      allowedOrigins.indexOf(origin) !== -1
+      //! remove in production
+      // || !origin
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not Allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
