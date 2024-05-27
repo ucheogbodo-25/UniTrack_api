4b23e5f35e2360c7692b7ad73135ed545b5595a8
ucheogbodo-25 <ucheogbodo25@gmail.com>
2024-05-27 13:53:55 -0700

    fixing bug

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,10 +1,19 @@
-const mongoose = require("mongoose");
+// dbconns.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.DATABASE_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully');
   } catch (err) {
-    console.log(err);
+    console.error('Database connection error:', err);
+    process.exit(1);
   }
 };
 
